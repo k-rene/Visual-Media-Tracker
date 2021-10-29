923229bcc62b49cb22a9a65a352bd6604a4978b7
k-rene <boeykr@hotmail.com>
2021-10-29 00:36:12 -0700

    lol jokes, *now* i've done task 5..  hopefully

--- src/main/ui/MediaTrackerApp.java
+++ src/main/ui/MediaTrackerApp.java
@@ -7,6 +7,8 @@ import model.Show;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,6 +22,7 @@ public class MediaTrackerApp {
     private Scanner input;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
+    private static final String JSON_STORE = "./data/medialist.json";
 
     public MediaTrackerApp() {
         runMediaTracker();
@@ -52,28 +55,66 @@ public class MediaTrackerApp {
         mediaList = new MediaList();
         input = new Scanner(System.in);
         input.useDelimiter("\n");
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     // EFFECTS: displays menu of options to user
     private void displayMenu() {
         System.out.println("\nChoose one:");
         System.out.println("\ta -> Add Media");
-        System.out.println("\te -> Update Media");
+        System.out.println("\tu -> Update Media");
         System.out.println("\tv -> View ALl Media");
+        System.out.println("\ts -> save media list to file");
+        System.out.println("\tl -> load media list from file");
         System.out.println("\tq -> Quit");
     }
 
     // MODIFIES: this
     // EFFECTS: processes user command from main menu
     private void processMenuCommand(String command) {
-        if (command.equals("a")) {
-            addMedia();
-        } else if (command.equals("e")) {
-            updateViewingStatus();
-        } else if (command.equals("v")) {
-            quickView();
-        } else {
-            System.out.println("Selection not valid...");
+        switch (command) {
+            case "a":
+                addMedia();
+                break;
+            case "u":
+                updateViewingStatus();
+                break;
+            case "v":
+                quickView();
+                break;
+            case "s":
+                saveMediaList();
+                break;
+            case "l":
+                loadMediaList();
+                break;
+            default:
+                System.out.println("Selection not valid...");
+                break;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads medialist from file
+    private void loadMediaList() {
+        try {
+            mediaList = jsonReader.read();
+            System.out.println("Loaded data from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
+    }
+
+    // EFFECTS: saves the medialist to file
+    private void saveMediaList() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(mediaList);
+            jsonWriter.close();
+            System.out.println("Saved data to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
         }
     }
 
@@ -122,7 +163,7 @@ public class MediaTrackerApp {
     private void addMovie() {
         // prompt for name, status etc, instantiate, then add to list
         String name;
-        Integer status;
+        int status;
         String platform;
 
         // prompt for movie name
@@ -145,9 +186,9 @@ public class MediaTrackerApp {
     private void addShow() {
         // prompt for name, status etc, instantiate, then add to list
         String name;
-        Integer status;
+        int status;
         String platform;
-        Integer bookmark;
+        int bookmark;
 
         // prompt for show name
         System.out.println("Please enter the name of the show:");
@@ -191,8 +232,8 @@ public class MediaTrackerApp {
     // MODIFIES: this
     // EFFECTS: updates the viewing status of a selected media
     private void updateViewingStatus() {
-        Integer index = -1;
-        Integer newStatus = -1;
+        int index = -1;
+        int newStatus = -1;
 
         quickView();
         System.out.println("Please select the index of the media to update");
@@ -210,8 +251,8 @@ public class MediaTrackerApp {
     private void quickView() {
         List<Media> list = mediaList.getList();
 
+        System.out.println("index" + "| title");
         for (int i = 0; i < mediaList.length(); i++) {
-            System.out.println("index" + "| title");
             System.out.println(i + "|" + list.get(i).getName());
         }
     }
