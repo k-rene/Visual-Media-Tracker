3f90707d004a819916938aa27300965da515781d
k-rene <boeykr@hotmail.com>
2021-11-07 22:42:14 -0800

    nothing runs but i think there's a rough skeleton now??

--- src/main/ui/MediaTrackerUI.java
+++ src/main/ui/MediaTrackerUI.java
@@ -5,10 +5,9 @@ package ui;
 // Alarm Controller: https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
 
 
-import model.Media;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -21,10 +20,11 @@ public class MediaTrackerUI extends JFrame {
     private JInternalFrame controlPanel;
 
     /**
-     * Constructor sets up ???
+     * Constructor sets up main menu panel
      */
     public MediaTrackerUI() {
-        //mta = new MediaTrackerApp();
+        // remove console UI stuff
+        // mta = new MediaTrackerApp();
 
         desktop = new JDesktopPane();
         desktop.addMouseListener(new DesktopFocusAction());
@@ -35,7 +35,24 @@ public class MediaTrackerUI extends JFrame {
         setTitle("Kae-Rene's Personal Project: First World Problems");
         setSize(WIDTH, HEIGHT);
 
+        addButtonPanel();
+        
         setVisible(true);
+    }
+
+    /**
+     * Helper to add control buttons.
+     */
+    private void addButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4,2));
+        buttonPanel.add(new JButton(new AddMediaAction()));
+        buttonPanel.add(new JButton(new UpdateMediaAction()));
+        buttonPanel.add(new JButton(new ViewAllMediaAction()));
+        buttonPanel.add(new JButton(new SaveMediaAction()));
+        buttonPanel.add(new JButton(new LoadMediaAction()));
+
+        controlPanel.add(buttonPanel, BorderLayout.WEST);
     }
 
     /**
@@ -49,8 +66,123 @@ public class MediaTrackerUI extends JFrame {
         }
     }
 
-    // Play the game
+    // Star the media tracker
     public static void main(String[] args) {
         new MediaTrackerUI();
+    }
+
+    /**
+     * Represents action to be taken when user wants to add a media
+     * to the system.
+     */
+    private class AddMediaAction extends AbstractAction {
+
+        AddMediaAction() {
+            super("Add Media");
+        }
+
+        @Override //STUB
+        public void actionPerformed(ActionEvent evt) {
+            // any set up things go here
+
+            try {
+                // do the process here
+            } catch (ActionFailedException e) { //
+                // exception thrown proecess here
+            }
+        }
+
+    }
+
+    /**
+     * Represents action to be taken when user wants to update a media
+     * to the system.
+     */
+
+    private class UpdateMediaAction extends AbstractAction {
+
+        UpdateMediaAction() {
+            super("Update Media");
+        }
+
+        @Override // STUB
+        public void actionPerformed(ActionEvent evt) {
+            // any set up things go here
+
+            try {
+                // do the process here
+            } catch (ActionFailedException e) { //MediaNotFoundException?
+                // exception thrown proecess here
+            }
+        }
+    }
+
+    /**
+     * Represents action to be taken when user wants to view all media
+     * to the system.
+     */
+
+    private class ViewAllMediaAction extends AbstractAction {
+
+        ViewAllMediaAction() {
+            super("View All Media");
+        }
+
+        @Override //STUB
+        public void actionPerformed(ActionEvent evt) {
+            // any set up things go here
+
+            try {
+                // do the process here
+            } catch (ActionFailedException e) {
+                // exception thrown proecess here
+            }
+        }
+    }
+
+    /**
+     * Represents action to be taken when user wants to save current media list
+     * to the system.
+     */
+
+    private class SaveMediaAction extends AbstractAction {
+
+        SaveMediaAction() {
+            super("Save Current");
+        }
+
+        @Override //STUB
+        public void actionPerformed(ActionEvent evt) {
+            // any set up things go here
+
+            try {
+                // do the process here
+            } catch (ActionFailedException e) {
+                // exception thrown proecess here
+            }
+        }
+    }
+
+    /**
+     * Represents action to be taken when user wants to load an existing media list
+     * to the system.
+     */
+
+    private class LoadMediaAction extends AbstractAction {
+
+        LoadMediaAction() {
+            super("Load Existing");
+        }
+
+        @Override //STUB
+        public void actionPerformed(ActionEvent evt) {
+            // any set up things go here
+
+            try {
+                // do the process here
+            } catch (ActionFailedException e) {
+                // exception thrown proecess here
+            }
+        }
     }
 }
--- src/main/ui/UI design notes.md
+++ src/main/ui/UI design notes.md
@@ -1,5 +1,33 @@
+###! to be removed before submission !
 ## process
 - move MediaTrackerApp menus into MediaTrackerUI as GUI
 - add mouse listener (desktop.addMouseListener) for menu navigation stuff
 - add key listener (addKeyListener) for field data inputs
-- 
+
+## code snippets
+### action template
+~~~
+/**
+* Represents action to be taken when user wants to <INSERT ACTION HERE>
+* to the system.
+*/
+
+private class *ActionName* extends AbstractAction {
+
+    AddCodeAction() {
+        super("ProcessLabel");
+    }
+    
+    @Override //STUB
+    public void actionPerformed(ActionEvent evt) {
+        // any set up things go here
+        
+        try {
+            // do the process here
+        } catch (ActionFailedException e) {
+           // exception thrown proecess here
+        }
+    }
+}
+~~~
+note: not all actionPerformed methods require a try-catch block
